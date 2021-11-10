af0ca719c40703a1e034c3e7efda6231f7c2f090
André Amaral <heyvoon@users.noreply.github.com>
2021-11-10 17:10:37 +0100

    Create Trim-Length.ps1

---
+++ Trim-Length.ps1
@@ -0,0 +1,7 @@
+function Trim-Length {
+param (
+    [parameter(Mandatory=$True,ValueFromPipeline=$True)] [string] $Str
+  , [parameter(Mandatory=$True,Position=1)] [int] $Length
+)
+    $Str[0..($Length-1)] -join ""
+}
